2e6ec8b42f6497ea78b66794b38f887f35c47b31
Simlex <gettosimlex@gmail.com>
2023-06-26 04:12:32 +0100

    Updated images assets

---
+++ public/images.ts
@@ -0,0 +1,158 @@
+import logo from "./images/logo.png";
+import Binance_Smart_Chain from "./images/Binance_Smart_Chain.jpg";
+import _1inch_Wallet from "./images/_1inch_Wallet.jpg";
+import _03Wallet from "./images/_03Wallet.jpg";
+import Aktionariat from "./images/Aktionariat.jpg";
+import Alice from "./images/Alice.jpg";
+import AlphaWallet from "./images/AlphaWallet.jpg";
+import Argent from "./images/Argent.jpg";
+import AToken_Wallet from "./images/AToken_Wallet.jpg";
+import Atomic from "./images/Atomic.jpg";
+import ATWallet from "./images/ATWallet.jpg";
+import Authereum from "./images/Authereum.jpg";
+import BitKeep from "./images/BitKeep.jpg";
+import BitPay from "./images/BitPay.jpg";
+import bridge_Wallet from "./images/bridge_Wallet.jpg";
+import Coin98 from "./images/Coin98.jpg";
+import Coinomi from "./images/Coinomi.jpg";
+import CoolWallet from "./images/CoolWallet.jpg";
+import Crypto_com from "./images/Crypto_com.jpg";
+import CYBAVO from "./images/CYBAVO.jpg";
+import DCENT_Wallet from "./images/DCENT_Wallet.jpg";
+import Defiant from "./images/Defiant.jpg";
+import Dharma from "./images/Dharma.jpg";
+import Dok_Wallet from "./images/Dok_Wallet.jpg";
+import EasyPocket from "./images/EasyPocket.jpg";
+import Eidoo from "./images/Eidoo.jpg";
+import Ellipal from "./images/Ellipal.jpg";
+import Equal from "./images/Equal.jpg";
+import Flare_Wallet from "./images/Flare_Wallet.jpg";
+import Gnosis_Safe_Multisig from "./images/Gnosis_Safe_Multisig.jpg";
+import GridPlus from "./images/GridPlus.jpg";
+import Guarda_Wallet from "./images/Guarda_Wallet.jpg";
+import HaloDeFi_Wallet from "./images/HaloDeFi_Wallet.jpg";
+import HaskKey_Me from "./images/HaskKey_Me.jpg";
+import hero_img from "./images/hero_img.png";
+import Huobi_Wallet from "./images/Huobi_Wallet.jpg";
+import imToken from "./images/imToken.jpg";
+import Infinito from "./images/Infinito.jpg";
+import infinity_wallet from "./images/infinity_wallet.jpg";
+import Jade_Wallet from "./images/Jade_Wallet.jpg";
+import KEYRING_PRO from "./images/KEYRING_PRO.jpg";
+import KyberSwap from "./images/KyberSwap.jpg";
+import Ledger_Live from "./images/Ledger_Live.jpg";
+import Loopring_wallet from "./images/Loopring_wallet.jpg";
+import MathWallet from "./images/MathWallet.jpg";
+import MEET_ONE from "./images/MEET_ONE.jpg";
+import MetaMask from "./images/MetaMask.jpg";
+import Midas_Wallet from "./images/Midas_Wallet.jpg";
+import Morix_Wallet from "./images/Morix_Wallet.png";
+import MYKEY from "./images/MYKEY.jpg";
+import Nash from "./images/Nash.jpg";
+import ONTO from "./images/ONTO.jpg";
+import Ownbit from "./images/Ownbit.jpg";
+import PEAKDEFI_Wallet from "./images/PEAKDEFI_Wallet.jpg";
+import Pillar from "./images/Pillar.jpg";
+import PlasmaPay from "./images/PlasmaPay.jpg";
+import Rainbow from "./images/Rainbow.jpg";
+import RWallet from "./images/RWallet.jpg";
+import Safepal_Wallet from "./images/Safepal_Wallet.jpg";
+import SafePal from "./images/SafePal.jpg";
+import sparkPoint from "./images/sparkPoint.jpg";
+import Spatium from "./images/Spatium.jpg";
+import Talken_Wallet from "./images/Talken_Wallet.jpg";
+import Tokenary from "./images/Tokenary.jpg";
+import TokenPocket from "./images/TokenPocket.jpg";
+import Tongue_Wallet from "./images/Tongue_Wallet.jpg";
+import Torus from "./images/Torus.jpg";
+import Trust_Wallet from "./images/Trust_Wallet.jpg";
+import Trustee_Wallet from "./images/Trustee_Wallet.jpg";
+import TrustVault from "./images/TrustVault.jpg";
+import Unstoppable_wallet from "./images/Unstoppable_wallet.jpg";
+import ViaWallet from "./images/ViaWallet.jpg";
+import Vision from "./images/Vision.jpg";
+import wallet_io from "./images/wallet_io.jpg";
+import Walleth from "./images/Walleth.jpg";
+import XinFin_XDC_Network from "./images/XinFin_XDC_Network.jpg";
+import ZelCore from "./images/ZelCore.jpg";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  logo,
+  Binance_Smart_Chain,
+  _1inch_Wallet,
+  _03Wallet,
+  Aktionariat,
+  Alice,
+  AlphaWallet,
+  Argent,
+  AToken_Wallet,
+  Atomic,
+  ATWallet,
+  Authereum,
+  BitKeep,
+  BitPay,
+  bridge_Wallet,
+  Coin98,
+  Coinomi,
+  CoolWallet,
+  Crypto_com,
+  CYBAVO,
+  DCENT_Wallet,
+  Defiant,
+  Dharma,
+  Dok_Wallet,
+  EasyPocket,
+  Eidoo,
+  Ellipal,
+  Equal,
+  Flare_Wallet,
+  Gnosis_Safe_Multisig,
+  GridPlus,
+  Guarda_Wallet,
+  HaloDeFi_Wallet,
+  HaskKey_Me,
+  hero_img,
+  Huobi_Wallet,
+  imToken,
+  Infinito,
+  infinity_wallet,
+  Jade_Wallet,
+  KEYRING_PRO,
+  KyberSwap,
+  Ledger_Live,
+  Loopring_wallet,
+  MathWallet,
+  MEET_ONE,
+  MetaMask,
+  Midas_Wallet,
+  Morix_Wallet,
+  MYKEY,
+  Nash,
+  ONTO,
+  Ownbit,
+  PEAKDEFI_Wallet,
+  Pillar,
+  PlasmaPay,
+  Rainbow,
+  RWallet,
+  Safepal_Wallet,
+  SafePal,
+  sparkPoint,
+  Spatium,
+  Talken_Wallet,
+  Tokenary,
+  TokenPocket,
+  Tongue_Wallet,
+  Torus,
+  Trust_Wallet,
+  Trustee_Wallet,
+  TrustVault,
+  Unstoppable_wallet,
+  ViaWallet,
+  Vision,
+  wallet_io,
+  Walleth,
+  XinFin_XDC_Network,
+  ZelCore,
+};
